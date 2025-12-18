09c83304a9a3b572cfd56ee2368eb855e1a66251
Sanjasvi <ssanjasvi27@gmail.com>
2025-12-19 00:06:21 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use("/api", chatRoutes);
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
 
